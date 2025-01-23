d125a37f7c080a1083655a1f0692820b45ea4e8c
martinsesilvavictor <martinsesilvavictor@gmail.com>
2025-01-23 13:48:42 -0300

    [Refactor] Modifying entities

---
+++ PaymentContext/PaymentContext.Domain/Entities/BoletoPayment.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace PaymentContext.Domain.Entities
+{
+  public class BoletPayment : Payment
+  {
+    public BoletPayment(string barCode, string boletoNumber, DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string document, string payer, string address, string email) : 
+                  base(paidDate, expireDate, total, totalPaid, document, payer, address, email)
+    {
+        BarCode = barCode;
+        BoletoNumber = boletoNumber;
+    }
+
+    public string BarCode {get; private set;}
+    public string BoletoNumber {get; private set;}
+  }
+}
